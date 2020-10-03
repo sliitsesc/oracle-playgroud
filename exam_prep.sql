80c01e9be5b6125174d006edf0c045c27cceb2f7
Chamod Shehanka <hcsperera@outlook.com>
2020-10-03 08:15:09 +0530

    Updated

--- exam_prep.sql
+++ exam_prep.sql
@@ -72,9 +72,49 @@ CREATE OR REPLACE TYPE BODY stock_type AS
 MEMBER FUNCTION yield RETURN FLOAT IS
     BEGIN
         RETURN ((SELF.individend/self.cprice) * 100);
-    END
+    END yield;
 END;
 
 SELECT s.company, s.yield()
 FROM stock_tbl s
 /
+
+-- Altering again to add new member function
+ALTER TYPE stock_type
+ADD MEMBER FUNCTION AUDtoUSD(rate FLOAT) RETURN FLOAT
+CASCADE
+/
+
+CREATE OR REPLACE TYPE BODY stock_type AS 
+MEMBER FUNCTION yield RETURN FLOAT IS
+    BEGIN
+        RETURN ((SELF.individend/self.cprice) * 100);
+    END yield;
+MEMBER FUNCTION AUDtoUSD(rate FLOAT) RETURN FLOAT IS
+    BEGIN
+        RETURN SELF.cprice * rate;
+    END AUDtoUSD;
+END;
+
+-- add new member function to get no of trades
+ALTER TYPE stock_type
+ADD MEMBER FUNCTION no_of_trades RETURN INTEGER
+CASCADE
+/
+
+CREATE OR REPLACE TYPE BODY stock_type AS
+MEMBER FUNCTION yield RETURN FLOAT IS
+    BEGIN
+        RETURN ((SELF.individend/self.cprice) * 100);
+    END yield;
+MEMBER FUNCTION AUDtoUSD(rate FLOAT) RETURN FLOAT IS
+    BEGIN
+        RETURN SELF.cprice * rate;
+    END AUDtoUSD;
+MEMBER FUNCTION no_of_trades RETURN INTEGER IS
+    BEGIN 
+        SELECT COUNT(e.column_value) INTO countt
+        FROM TABLE(SELF.ex_traded) e;
+        RETURN countt;
+    END no_of_trades;
+END
